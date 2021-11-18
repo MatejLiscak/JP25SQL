6beb7ae53f34cb9156d6f8f67f1a43a183d911bf
Matej Liscak <liscakmatej1@gmail.com>
2021-11-18 11:13:53 +0100

    Vjezba zadatak zastita zivotinja

---
+++ zastitazivotinja.sql
@@ -0,0 +1,28 @@
+drop database if exists zastitazivotinja;
+create database zastitazivotinja character set utf8;
+use zastitazivotinja;
+
+create table osoba(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    IBAN varchar(50) not null
+);
+
+create table prostor(
+    sifra int not null primary key auto_increment,
+    kapacitet varchar(50) not null,
+    popunjen boolean
+);
+
+create table zivotinja(
+    osoba int not null,
+    prostor int not null,
+    vrsta varchar(50) not null,
+    tezina varchar(50) not null,
+    visina varchar(50) not null
+
+);
+
+alter table zivotinja add foreign key (osoba) references osoba(sifra);
+alter table zivotinja add foreign key (prostor) references prostor(sifra);
