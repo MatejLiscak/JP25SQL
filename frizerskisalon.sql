8245d53af597dfdeebfd33a3fd1ce135d58cb47d
Matej Liscak <liscakmatej1@gmail.com>
2021-11-18 11:26:23 +0100

    Dodani atributi, insert i delete komande

--- frizerskisalon.sql
+++ frizerskisalon.sql
@@ -19,8 +19,26 @@ create table korisnici(
 
 create table usluge(
    sifra int not null primary key auto_increment,
-   naziv varchar(50)
+   naziv varchar(50) not null,
+   cijena varchar(50) not null,
+   trajanje varchar(50) not null
 );
 
 alter table korisnici add foreign key (djelatnica) references djelatnica(sifra);
 alter table korisnici add foreign key (usluge) references usluge(sifra);
+
+# 1-3
+insert into djelatnica (sifra,ime,prezime) values
+(null,'Branka','Jordić'),
+(null,'Marina','Posnić'),
+(null,'Korina','Izok');
+
+# 1-4
+insert into usluge(sifra,naziv,cijena,trajanje) values
+(null,'Farbanje',150,60),
+(null,'Muško šišanje',45,30),
+(null,'Žensko šišanje',60,45),
+(null,'Pramenovi',100,45);
+
+delete from usluge where sifra=4;
+
