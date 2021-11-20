d8f8d4980c147409021ec72ad1ab4b8cc88e92c9
Matej Liscak <liscakmatej1@gmail.com>
2021-11-20 17:53:41 +0100

    insert i update

--- zastitazivotinja.sql
+++ zastitazivotinja.sql
@@ -27,4 +27,22 @@ create table zivotinja(
 alter table zivotinja add foreign key (osoba) references osoba(sifra);
 alter table zivotinja add foreign key (prostor) references prostor(sifra);
 
+insert into osoba(sifra,ime,prezime,IBAN)values
+(null,'Ivo','Josipović','HR3724840082773533972'),
+(null,'Mara','Karimović','HR5824020069614565167');
+
+insert into prostor(sifra,kapacitet)values
+(null,50),
+(null,70),
+(null,89);
+
+insert into zivotinja(osoba,prostor,vrsta,tezina,visina)values
+(1,3,'pas',45,70),
+(2,1,'mačka',10,20),
+(2,2,'krava',400,100);
+
+update prostor set kapacitet=100 where sifra=2;
+
+
+
 
