1dd88b816e9561d61b69ebd4414c81dc3595f9a2
Matej Liscak <liscakmatej1@gmail.com>
2021-11-23 19:27:29 +0100

    Ispravak završnog rada

--- cryptocompanion.sql
+++ cryptocompanion.sql
@@ -1,39 +1,44 @@
-drop database if exists cryptocompanion;
-create database cryptocompanion character set utf8;
-use cryptocompanion;
+drop database if exists portfolio;
+create database portfolio character set utf8;
+use portfolio;
 
-create table holder(
+create table korisnik(
     sifra int not null primary key auto_increment,
     ime varchar(50) not null,
     prezime varchar(50) not null,
-    email varchar (50)
+    email varchar(50) not null
+);
+
+create table novcanik(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    korisnik int not null,
+    vrijednost varchar(50)
 );
 
 create table kriptovaluta(
     sifra int not null primary key auto_increment,
-    naziv varchar(50) not null,
-    kolicina decimal (18,2) not null,
-    kupovnacijena decimal(18,2) not null,
-    prodajnacijena decimal (18,2),
-    profit decimal (18,2) not null
+    naziv varchar(50),
+    oznaka varchar(50),
+    marketcap decimal(18.2),
+    transakcija int not null,
+    novcanik int not null
 );
 
-create table portfolio(
-    naziv varchar(50) not null,
-    kriptovaluta int not null,
-    holder int not null
+create table transakcija(
+    sifra int not null primary key auto_increment,
+    exchange int not null,
+    vrijednost decimal(18.2),
+    oznakakriptovalute varchar(50)
 );
 
-alter table portfolio add foreign key (kriptovaluta) references kriptovaluta(sifra);
-alter table portfolio add foreign key (holder) references holder(sifra);
+create table exchange(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    certificiran boolean
+);
 
-#1
-insert into holder (sifra,ime,prezime,email)
-values(null,'Matej','Liscak','liscakmatej1@gmail.com');
-
-#1
-insert into kriptovaluta (sifra,naziv,kolicina,kupovnacijena,prodajnacijena)
-values (null,'Bitcoin',3,32145.24,64568.31);
-
-insert into portfolio (naziv,kriptovaluta,holder)
-values('Main',1,1);
+alter table novcanik add foreign key (korisnik) references korisnik(sifra);
+alter table kriptovaluta add foreign key (transakcija) references transakcija(sifra);
+alter table kriptovaluta add foreign key (novcanik) references novcanik(sifra);
+alter table transakcija add foreign key (exchange) references exchange(sifra);
